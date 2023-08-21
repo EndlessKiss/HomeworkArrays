aaace4fec11dc845a50a337539041dc4aa3c5095
Anastasia Anisimova <pin_up@bk.ru>
2023-08-22 02:03:36 +0300

    дз

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,57 @@
+import java.util.Arrays;
+import java.util.OptionalDouble;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int sum = Arrays.stream(generateRandomArray()).sum();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        int max = Arrays.stream(generateRandomArray())
+                .max()
+                .getAsInt();
+
+        int min = Arrays.stream(generateRandomArray())
+                .min()
+                .getAsInt();
+
+        System.out.println("Минимальная сумма трат составила " + min + " рублей");
+        System.out.println("Максимальная сумма трат составила " + max + " рублей");
+
+        double summ = 0;
+        for (int x: generateRandomArray()) {
+            summ += x;
         }
+        System.out.println("Средняя сумма трат за месяц составила " + summ/generateRandomArray().length + " рублей");
+
+
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        String result = "";
+        for (int i = reverseFullName.length - 1; i >= 0; i--) {
+            result = result + reverseFullName[i];
+                   }
+        System.out.println(result);
     }
-}
+
+
+
+
+    int[] arr = generateRandomArray();
+
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+
+    }
+
+    }
+
+
